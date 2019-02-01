adff0a5cb1ae81b7d2fd841b70dbfd50714c0c84
Tracy Reith <treith@treith.com>
2019-02-01 10:15:54 -0800

    Confirm that js prototype to sum cost of pizza size and topping works

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,34 @@ function Pizza() {
   this.toppings = [];
 }
 
-Pizza.prototype.price = function(size, topping) {
-  price = this.size + this.topping;
-  return price;
+function SizeCost() {
+  large = 16;
+  medium = 12;
+  small = 8;
 }
 
+function ToppingCost() {
+  mushrooms = 2;
+  olives = 2;
+  pepperoni = 3;
+}
 
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping);
+}
+
+Pizza.prototype.price = function(sizeCost, toppingCost) {
+  return sizeCost + toppingCost;
+}
+
+// cost
+// small : 8
+// medium : 12
+// large : 16
+//
+// pepperoni : 3
+// olives : 2
+// mushrooms : 2
 //** UI LOGIC
 
 $(document).ready(function(){
